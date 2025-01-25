70311c0312e23cda8a23f54fd07ec22135d5a2cf
Glenhawk-0 <LennonEhare@Gmail.com>
2025-01-25 15:33:10 -0500

    Did greeter1 and 2 but not more Greeter

--- Hare-Lennon-pe02/greeter-modules/src/main.js
+++ Hare-Lennon-pe02/greeter-modules/src/main.js
@@ -7,14 +7,20 @@
 // 3B - get a reference to the #output <p>
 // 3C - update HTML of #output <p>
 
-import { sayHello, doubleIt, defaultName, meaningOfLife } from "./utils.js";
+import { sayHello, doubleIt, defaultFirstName, defaultLastName, meaningOfLife } from "./utils.js";
 import * as utils from  "./utils.js"; // OR give all of the exported `utils` functions a namespace
+
+window.onload 
 
 let temp = "main.js temp value"; // does not conflict with `temp` in utils.js
 
-const input = document.querySelector("#input-firstname");
+const input1 = document.querySelector("#input-firstname");
+const input2 = document.querySelector("#input-lastname");
 const output = document.querySelector("#output");
 const cbForcefully = document.querySelector("#cb-forcefully");
+
+output.style.background = 'yellow';
+output.style.color = 'red';
 
 const helloButton = document.querySelector("#btn-hello");
 const goodbyeButton = document.querySelector("#btn-goodbye");
@@ -23,13 +29,14 @@ let forcefully = cbForcefully.checked;
 
 //cbForcefully.onchange = () => forcefully = cbForcefully.checked;
 cbForcefully.onchange = e => forcefully = e.target.checked;
-helloButton.onclick = () => output.innerHTML = sayHello("Hello",input.value.trim(),forcefully);
-goodbyeButton.onclick = () => output.innerHTML = sayHello("Goodbye",input.value.trim(),forcefully);
+helloButton.onclick = () => output.innerHTML = sayHello("Hello",input1.value.trim(), input2.value.trim() ,forcefully);
+goodbyeButton.onclick = () => output.innerHTML = sayHello("Goodbye",input1.value.trim(),input2.value.trim() ,forcefully);
 
 console.log("sayHello('Hey There') = ", sayHello('Hey there'));
 console.log("doubleIt(10) = ", doubleIt(10));
-console.log("defaultName = ", defaultName); // FAILS - we need to import it
+console.log("defaultName = ", defaultFirstName); // FAILS - we need to import it
 //console.log("meaningOfLife = ", meaningOfLife); // FAILS - it is not being exported by utils.js
 
 console.log("temp = ", temp);
 console.log("utils.temp = ", utils.temp); // named import
+
